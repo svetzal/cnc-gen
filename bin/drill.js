e199ad47bcf0f878cb382f455ec21261d8653ef5
Stacey Vetzal <stacey@vetzal.com>
2021-04-11 21:17:03 -0400

    command line improvements

--- bin/drill.js
+++ bin/drill.js
@@ -1,20 +1,94 @@
 #!/usr/bin/env node
 
-const {createDrillingProgram} = require("../lib/drilling_generator");
-const {DefaultSettings} = require("../lib/default_settings");
-const {CommandGenerator} = require("../lib/command_generator");
-const {ConsoleWriter, Vector} = require('../lib/utilities');
+const yargs = require('yargs');
 
-const main = async () => {
+const {createDrillingProgram} = require("../lib/drilling_generator");
+const {DefaultSettings, createSettingsFromArgs} = require("../lib/default_settings");
+const {CommandGenerator} = require("../lib/command_generator");
+const {ConsoleWriter, FileWriter, Vector} = require('../lib/utilities');
+
+const args = require('yargs')
+    .scriptName('cnc-gen-drill')
+    // Settings
+    .option('r', {
+        alias: 'retractHeight',
+        default: DefaultSettings.retractHeight,
+        describe: 'retraction height for rapid moves (mm)',
+        type: 'number',
+    })
+    .option('f', {
+        alias: 'normalCutFeedRate',
+        default: DefaultSettings.normalCutFeedRate,
+        describe: 'feed rate when cutting normal overlap passes (mm/min)',
+        type: 'number',
+    })
+    .option('ff', {
+        alias: 'firstCutFeedRate',
+        default: DefaultSettings.firstCutFeedRate,
+        describe: 'feed rate when cutting full mill diameter (mm/min)',
+        type: 'number',
+    })
+    .option('fs', {
+        alias: 'stepDownFeedRate',
+        default: DefaultSettings.stepDownFeedRate,
+        describe: 'feed rate with helical step down (mm/min)',
+        type: 'number',
+    })
+    .option('s', {
+        alias: 'stepDown',
+        default: DefaultSettings.stepDownRate,
+        describe: 'total step down amount (mm)',
+        type: 'number',
+    })
+    .option('m', {
+        alias: 'safetyMargin',
+        default: DefaultSettings.safetyMargin,
+        describe: 'safety margin to back away from edges and avoid hitting stock (mm)',
+        type: 'number',
+    })
+    // General
+    .option('o', {
+        alias: 'output',
+        describe: 'write cnc program to this filename instead of console',
+        type: 'string',
+    })
+    // Drill
+    .option('t', {
+        alias: 'toolDiameter',
+        default: 6.35,
+        describe: 'mill bit diameter (mm)',
+        type: 'number',
+    })
+    .option('d', {
+        alias: 'diameter',
+        default: 6.35 * 0.3,
+        describe: 'mill bit diameter (mm)',
+        type: 'number',
+    })
+    .option('z', {
+        alias: 'depth',
+        default: 1,
+        describe: 'depth to drill (mm)'
+    })
+    .help()
+    .argv;
+
+const main = async (args) => {
+    let settings = createSettingsFromArgs(args);
+
+    let writer = args.output
+        ? new FileWriter(args.output)
+        : new ConsoleWriter();
+
     createDrillingProgram(
-        new ConsoleWriter(),
-        DefaultSettings,
+        writer,
+        settings,
         new CommandGenerator(),
-        6.35,
+        args.toolDiameter,
         new Vector(0, 0),
-        6.35 * 0.3,
-        0.3,
+        args.diameter,
+        0 - args.depth,
     );
 }
 
-main();
+main(args);
